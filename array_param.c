5864c95fc31c0cce6ac72df2a4ef6c1b542b8f1e
Ssuching Yu <ssuchingyu@gmail.com>
2024-09-17 00:00:07 +0800

    Update array decay in function parameter example.

--- array_param.c
+++ array_param.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
 // The innermost dimension of an array parameter to a function is lost.
-int array_size(double a[]) {
-    return sizeof(a);
+void array_size(double a[])
+{
+    printf("sizeof(v) inside function: %ld\n", sizeof(a));
+}
+
+// Only the innermost dimension of an array parameter is rewritten.
+void array_size_2d(size_t n, size_t m,
+                    double a[n][m])
+{
+    printf("sizeof(a) inside function: %ld\n", sizeof(a));
+    printf("sizeof(a[0]) inside function: %ld\n", sizeof(a[0]));
+    printf("sizeof(*a) inside function: %ld\n", sizeof(*a));
 }
 
 int main() {
+    double v[] = {1.0, 2.0, 3.0};
+    printf("sizeof(v): %ld\n", sizeof(v));
+    array_size(v);
 
-    double a[] = { 1.0, 2.0, 3.0};
+    double a[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    printf("sizeof(a): %ld\n", sizeof(a));
+    array_size_2d(2, 3, a);
 
-    printf("Size of array a: %d\n", sizeof(a));
-    printf("array_size(a): %d\n", array_size(a));
-    
     return 0;
 }
